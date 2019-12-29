7843060646affa8dfb779406a9dcdbdd089eccad
anthonyvigz <anthonyvigz@gmail.com>
2019-12-29 14:17:14 -0500

    set up images in public folder and put them in array

---
+++ mainsite/src/photos.js
@@ -0,0 +1,188 @@
+const portfolio = [
+    {
+      src: "/Portfolio/chila.jpg",
+      name: "chile"
+    },
+    {
+      src: "/Portfolio/2019 Cover_Insert.jpg",
+      name: "cover"
+    },
+    {
+      src: "/Portfolio/AIG RepGuard.jpg",
+      name: "AIG"
+    },
+    {
+      src: "/Portfolio/AugustasFoodTruck.jpg",
+      name: "augustas"
+    },
+    {
+      src: "/Portfolio/BC High Gift Giving-1.jpg",
+      name: "BC"
+    },
+    {
+      src: "/Portfolio/BC.jpg",
+      name: "BC2"
+    },
+    {
+      src: "/Portfolio/Brookstone.jpg",
+      name: "Brookstone"
+    },
+    {
+      src: "/Portfolio/Cubs Destinations Logo.jpg",
+      name: "Cubs"
+    },
+    {
+      src: "/Portfolio/Cyber_cover.jpg",
+      name: "cyber"
+    },
+    {
+      src: "/Portfolio/dargent.jpg",
+      name: "dargent"
+    },
+    {
+      src: "/Portfolio/Enterprise Ad FPAF.jpg",
+      name: "enterprise"
+    },
+    {
+      src: "/Portfolio/FPAF.jpg",
+      name: "FPAF"
+    },
+    {
+      src: "/Portfolio/Frost Club.jpg",
+      name: "FrostClub"
+    },
+    {
+      src: "/Portfolio/gold Cap Spasso.jpg",
+      name: "gold cap"
+    },
+    {
+      src: "/Portfolio/HGConstruction.jpg",
+      name: "HG"
+    },
+    {
+      src: "/Portfolio/Howl April-1.jpg",
+      name: "Howl"
+    },
+    {
+      src: "/Portfolio/Howl Magazine Holiday.jpg",
+      name: "howlmag"
+    },
+    {
+      src: "/Portfolio/IIMStuffer V7_Retirement Stuffer-1.jpg",
+      name: "retire"
+    },
+    {
+      src: "/Portfolio/In House Debit.Sage5.jpg",
+      name: "sage"
+    },
+    {
+      src: "/Portfolio/L5Cover.jpg",
+      name: "L5"
+    },
+    {
+      src: "/Portfolio/Long Way Top.jpg",
+      name: "longway"
+    },
+    {
+      src: "/Portfolio/M5.jpg",
+      name: "M5"
+    },
+    {
+      src: "/Portfolio/mswhome.jpg",
+      name: "msw"
+    },
+    {
+      src: "/Portfolio/OCD Retro Postcard.jpg",
+      name: "OCD"
+    },
+    {
+      src: "/Portfolio/Outcomes & Impact Layout-1.jpg",
+      name: "outcomes"
+    },
+    {
+      src: "/Portfolio/PE TOP.jpg",
+      name: "PE"
+    },
+    {
+      src: "/Portfolio/Poca Poster.jpg",
+      name: "POCA"
+    },
+    {
+      src: "/Portfolio/PocaLogo.jpg",
+      name: "pocalogo"
+    },
+    {
+      src: "/Portfolio/Product Shot.jpg",
+      name: "productShot"
+    },
+    {
+      src: "/Portfolio/PuppyEmporium Logo.jpg",
+      name: "puppy"
+    },
+    {
+      src: "/Portfolio/Red Sox CR Cover.jpg",
+      name: "redsox"
+    },
+    {
+      src: "/Portfolio/rockingham2.jpg",
+      name: "rock"
+    },
+    {
+      src: "/Portfolio/Rubinoff.jpg",
+      name: "rubinoff"
+    },
+    {
+      src: "/Portfolio/Sage_mobile-banking.jpg",
+      name: "sage1"
+    },
+    {
+      src: "/Portfolio/sagereport.jpg",
+      name: "sagereport"
+    },
+    {
+      src: "/Portfolio/SourPeach.jpg",
+      name: "sour"
+    },
+    {
+      src: "/Portfolio/Spinner Auction.jpg",
+      name: "spinner"
+    },
+    {
+      src: "/Portfolio/StrideRite.jpg",
+      name: "stride"
+    },
+    {
+      src: "/Portfolio/SumNews Fall.jpg",
+      name: "sumnews"
+    },
+    {
+      src: "/Portfolio/sweet.jpg",
+      name: "sweet"
+    },
+    {
+      src: "/Portfolio/tl5 final logo.jpg",
+      name: "tl5"
+    },
+    {
+      src: "/Portfolio/Triangle.jpg",
+      name: "triangle"
+    },
+    {
+      src: "/Portfolio/tuscan group2.jpg",
+      name: "tusc"
+    },
+    {
+      src: "/Portfolio/Tuscan Logo.jpg",
+      name: "tusc2"
+    },
+    {
+      src: "/Portfolio/Tuscan tin.jpg",
+      name: "tusc3"
+    },
+    {
+      src: "/Portfolio/Viking Tuscan Style.jpg",
+      name: "tusc4"
+    },
+]
+
+export default portfolio;
--- mainsite/src/views/Work.jsx
+++ mainsite/src/views/Work.jsx
@@ -2,30 +2,22 @@ import React, { Component } from 'react';
 import AbsoluteWrapper from './AbsoluteWrapper';
 import '../css/work.css';
 import { Spring } from 'react-spring/renderprops';
-import Coverflow from 'react-coverflow';
-var listOfImages = [];
+import portfolio from '../photos';
 
 class Work extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      photos: [
-          
-      ]
+      photos: portfolio
     }
-  }
-
-  importAll(r) {
-        return r.keys().map(r);
-    }
-    
-  componentWillMount() {
-      listOfImages = this.importAll(require.context('../img/Portfolio', false, /\.(png|jpe?g|svg)$/));
   }
 
   render() {
     return (
       <AbsoluteWrapper>
+        {this.state.photos.map( (photo, index ) => {
+          return <a href={photo.src}><img src={photo.src} alt={photo.name} width="300px" /></a>
+        })}
       </AbsoluteWrapper>
     );
   }
